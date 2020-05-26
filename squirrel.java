3a0b2aede848e5fe20c8b325686076e7ca3fb142
esonegin <65952547+esonegin@users.noreply.github.com>
2020-05-26 15:31:40 +0300

    Update squirrel.java

--- squirrel.java
+++ squirrel.java
@@ -1,15 +1,9 @@
-public static class Squirell{
-        
-        public int factnuts(int n) throws Exception{
-            if(n < 0){
-                throw new Exception();
-            }
-            
+public class Level1{
+        public static int factnuts(int n) {
         int result = 1;
 		for (int i = 1; i <= n; i ++){
 			result = result*i;
 		}
-		
 		return Integer.parseInt(String.valueOf(result).substring(0, 1));
         }
     }
