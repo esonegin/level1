8842de608c027280ec373a16831b0c03b7cbc95d
esonegin <65952547+esonegin@users.noreply.github.com>
2020-05-27 11:41:52 -0700

    Update ConquestCampaign.java

--- ConquestCampaign.java
+++ ConquestCampaign.java
@@ -1,22 +1,38 @@
 import java.util.*;
 
-public class Level1 {
+public static class Level1 {
         public static int ConquestCampaign(int N, int M, int L, int [] battalion) {
             int result = 0;
             int control = 0;
-            for(int day = 1; control < N * M; day++){
-                if(day == 1){
+            for (int day = 1; control < N * M; day++) {
+                if (day == 1) {
                     control = L;
-                }
-                else{
-                    control = control + (FourCell(N, M, battalion) * 4
+                } else {
+                    control = control + FourCell(N, M, battalion) * 4
                             + ThreeCell(N, M, battalion) * 3
-                            + TwoCell(N, M, battalion) * 2);
-
+                            + TwoCell(N, M, battalion) * 2;
                 }
                 result = day;
             }
 
+            return result;
+        }
+
+        public static int[] CellDublFilter(int[] battalion) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i < battalion.length - 3; i += 2)
+                if (battalion[i] == battalion[i + 2] && battalion[i + 1] == battalion[i + 3]) {
+                    continue;
+                } else if (i < battalion.length - 1) {
+                    list.add(battalion[i]);
+                    list.add(battalion[i+1]);
+            }
+            list.add(battalion[battalion.length - 2]);
+            list.add(battalion[battalion.length - 1]);
+            int[] result = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                result[i] = list.get(i);
+            }
             return result;
         }
 
@@ -29,6 +45,7 @@ public class Level1 {
             }
             return four;
         }
+
 
         public static int ThreeCell(int N, int M, int[] battalion) {
             int two = 0;
@@ -52,4 +69,5 @@ public class Level1 {
             }
             return three;
         }
+    }
     }
