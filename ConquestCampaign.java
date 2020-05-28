80ec7584091c8c90ba20d8dff756512dffa8a46b
esonegin <65952547+esonegin@users.noreply.github.com>
2020-05-28 22:11:18 +0300

    Update ConquestCampaign.java

--- ConquestCampaign.java
+++ ConquestCampaign.java
@@ -21,26 +21,39 @@ public class Level1 {
 
         public static int[] CellDublFilter(int[] battalion) {
             ArrayList<Integer> list = new ArrayList<>();
+
+            //Сравниваем текущую пару цифр со следующей и добавляем в список, если они не равны
             for (int i = 0; i < battalion.length - 3; i += 2)
                 if (battalion[i] == battalion[i + 2] && battalion[i + 1] == battalion[i + 3]) {
                     continue;
                 } else if (i < battalion.length - 1) {
                     list.add(battalion[i]);
                     list.add(battalion[i + 1]);
+
                 }
+            if (list.size() == 0) {
+                list.add(battalion[0]);
+                list.add(battalion[0]);
+            }
+
+            //Сравниваем последнюю пару с остальными и добавляем в список, если они не равны
             for(int j = 1; j < battalion.length-1; j++){
+                if(list.size() < 4){
+                    break;
+                }
                 if(battalion[battalion.length-2] == list.get(j) && battalion[battalion.length-1] == list.get(j)){
                     list.remove(j);
                     list.remove(j+1);
                     continue;
                 }
-                else{
+                else {
                     list.add(battalion[battalion.length-2]);
                     list.add(battalion[battalion.length-1]);
                     break;
                 }
             }
 
+            //Переводим список в массив
             int[] result = new int[list.size()];
             for (int i = 0; i < list.size(); i++) {
                 result[i] = list.get(i);
