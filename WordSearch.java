ad72e201b87afa520fbb0153434f1db7ffce3a32
esonegin <65952547+esonegin@users.noreply.github.com>
2020-06-09 23:15:38 +0300

    Update WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -3,29 +3,54 @@ import java.util.*;
 
 public class Level1 {
         public static int [] WordSearch(int len, String s, String subs) {
-            int[] result = new int[0];
+            int[] result;
+
+            //Разбиваем строку по ширине переносом
             String razbienie = Razbienie(s, len);
+
+            //Создаем массив подстрок по признаку перехода на новую строку
             String[] podstroki = razbienie.split(System.lineSeparator());
-            result = SearchInArray(podstroki, subs);
+
+            if (s.contains(" ")) {
+                result = SearchInArrayWithProbel(podstroki, subs);
+            } else {
+                result = SearchInArrayWithoutProbel(podstroki, subs);
+            }
             return result;
         }
 
-        //Считаем количество вхождений искомой строки в массив
-        public static int[] SearchInArray(String[] podstroki, String subs) {
-
+        //Считаем количество вхождений искомой строки в массив для строки с пробелом
+        public static int[] SearchInArrayWithProbel(String[] podstroki, String subs) {
             int[] result = new int[podstroki.length];
+
+
             for (int i = 0; i < podstroki.length; i++) {
                 if (podstroki[i].contains(" " + subs + " ")
-                        || podstroki[i].contains("\n" + subs + "\n")
-                        || podstroki[i].contains(subs + " ")
                         || podstroki[i].contains(" " + subs + "\n")
-                        || podstroki[i].contains(subs + "\n")) {
+                        || podstroki[i].contains("\n" + subs + "\n")
+                        || podstroki[i].contains("\n" + subs + " ")
+                        || podstroki[i].contains("" + subs + " ")) {
                     result[i] = 1;
-                } else {
-                    result[i] = 0;
-                }
+                } else result[i] = 0;
             }
+            return result;
+        }
 
+        //Считаем количество вхождений искомой строки в массив для строки без пробела
+        public static int[] SearchInArrayWithoutProbel(String[] podstroki, String subs) {
+            int[] result = new int[podstroki.length];
+
+
+            for (int i = 0; i < podstroki.length; i++) {
+                if (podstroki[i].contains(" " + subs + " ")
+                        || podstroki[i].contains(" " + subs + "\n")
+                        || podstroki[i].contains("\n" + subs + "\n")
+                        || podstroki[i].contains("\n" + subs + " ")
+                        || podstroki[i].contains(" " + subs + "")
+                        || podstroki[i].contains("" + subs + " ")) {
+                    result[i] = 1;
+                } else result[i] = 0;
+            }
             return result;
         }
 
@@ -33,6 +58,21 @@ public class Level1 {
         //Разбиваем строку по ширине
         public static String Razbienie(String s, int len) {
             Pattern p = Pattern.compile("\\s*(?:(\\w{10,})|(.{1,12})(?!\\w))\\s*", Pattern.UNICODE_CHARACTER_CLASS);
-            return (p.matcher(s).replaceAll("$1$2\n"));
+            String first = (p.matcher(s).replaceAll("$1$2\n"));
+
+            String second = (p.matcher(s).replaceAll("$1$2"));
+            String finalstring = "";
+
+            if (s.contains(" ")) {
+                finalstring = first;
+
+            } else {
+                String newStr = second.replaceAll("(.{" + len + "})", "$1|");
+                String[] newStrings = newStr.split("\\|");
+                for (int j = 0; j < newStrings.length; j++) {
+                    finalstring += " " + newStrings[j] + "\n";
+                }
+            }
+            return finalstring;
         }
     }
