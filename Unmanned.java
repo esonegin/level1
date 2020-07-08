956e2fdd708f5b057a3f47a6b45762e7a7e1cef0
esonegin <65952547+esonegin@users.noreply.github.com>
2020-07-08 17:56:24 +0300

    Update Unmanned.java

--- Unmanned.java
+++ Unmanned.java
@@ -1,46 +1,36 @@
 import java.util.*;
-
+//v2
 
 public class Level1 {
 
     public static int Unmanned(int L, int N, int[][] track) {
         int time = 0;
-        int rasstoyanie = 0;
+        Svetofors[] svetofors = arraySvetofors(track);
         if (track.length == 0) {
             time = L;
+        } else if (track.length != 0) {
 
-
-        } else {
-            Svetofors[] svetofors = arraySvetofors(track);
-            //Проходим цикл, пока расстояние не станет равно длинне пути
-
-            while (rasstoyanie < L) {
-
-                //Проходим цикл по светофорам
-                for (int i = 0; i < svetofors.length; i++) {
-                    //Прибавляем время и расстояние, если не находимся на светофоре
-                    if (rasstoyanie != svetofors[i].mesto) {
-                        rasstoyanie++;
-                        time++;
+            for (int rasstoyanie = 0, nomer = 0; rasstoyanie < L; ) {
+                if (rasstoyanie == svetofors[nomer].mesto
+                        && svetofors[nomer].TimeStep(time, svetofors[nomer].redtime, svetofors[nomer].greentime).equals("red")) {
+                    time++;
+                    continue;
+                } else if (rasstoyanie == svetofors[nomer].mesto
+                        && svetofors[nomer].TimeStep(time, svetofors[nomer].redtime, svetofors[nomer].greentime).equals("green")) {
+                    time++;
+                    rasstoyanie++;
+                    if (nomer + 1 < svetofors.length) {
+                        nomer++;
                     }
-                    //Прибавляем время и расстояние, если включился зеленый
-                    else if (rasstoyanie == svetofors[i].mesto
-                            && (svetofors[i].TimeStep(time, svetofors[i].redtime, svetofors[i].greentime).equals("green"))) {
-                        rasstoyanie++;
-                        time++;
-                        continue;
-                    }
-                    //Прибавляем только время, если светофор красный
-                    else if (rasstoyanie == svetofors[i].mesto
-                            && (svetofors[i].TimeStep(time, svetofors[i].redtime, svetofors[i].greentime).equals("red"))) {
-                        time++;
-                    }
-                    break;
+
+                } else if (rasstoyanie != svetofors[nomer].mesto) {
+                    time++;
+                    rasstoyanie++;
+                    continue;
                 }
             }
         }
         return time;
-
     }
 
 
@@ -55,84 +45,58 @@ public class Level1 {
             this.greentime = green;
         }
 
-        //Цвет светофора в момент времени
         public String TimeStep(int time, int red, int green) {
             String result = null;
+            int res = 0;
 
-            //Дефолтный красный
             if (time <= red - 1) {
                 result = "red";
             }
 
-            //Дефолтный зеленый
-            if (time >= red && time < red + green) {
+            else if (time >= red && time < red + green) {
                 result = "green";
             }
 
-            //Прибавляем к 0 диапазоны, пока не получим фактическое время
-            else if (time > red + green) {
-                int res = 0;
-                int i = 0;
-
-                //Пока результат меньше времени
-                while (res < time) {
-                    //Прибавляем время красного светофора
+            else {
+                while (res <= time) {
                     res += red;
-                    //Если результат больше или равен времени, то прибавляем нечетный диапазон и заканчиваем цикл
-                    if (res >= time) {
-                        i++;
-                        break;
-                    }
-
-                    //Если результат меньше времени, то прибавляем четный цикл
-                    else if (res < time) {
+                    if (res < time) {
                         res += green;
-                        i++;
-                    }
-
-                    //Если реузльтат больше или равен времени, то заканчиваем цикл
-                    if (res >= time) {
+                        continue;
+                    } else if (res >= time) {
                         break;
                     }
-
-                    //Если результат меньше времени, то продолжаем цикл
-                    else continue;
-
                 }
 
-                //Если циклов четное количество, то светофор горит зеленым
-                if (res % 2 == 0) {
+                if (res % 2 == 0 && res < time) {
                     result = "green";
-
-                    //Если циклов нечетное количество, то светофор горит красным
-                } else if (res % 2 != 0) {
+                }
+                if (res % 2 == 0 && res == time + 1 && mesto % 2 == 0) {
                     result = "red";
+                }
+                else if (res % 2 == 0 && res > time && mesto % 2 != 0) {
+                    result = "green";
+                }
+                else if (res % 2 == 0 && res == time) {
+                    result = "green";
+                }
+
+                else if (res % 2 != 0 && res < time) {
+                    result = "red";
+                }
+                else if (res % 2 != 0 && res > time) {
+                    result = "red";
+                }
+                else if (res % 2 != 0 && res == time) {
+                    result = "green";
                 }
 
             }
             return result;
         }
+
     }
 
-    public static int DlinnaDoPervogoSvetofora(int[][] track) {
-        return track[0][0];
-    }
-
-    public static int DlinnaOtPoslednegoDoKonca(int L, int[][] track) {
-        int posledniy = track[(track.length - 1)][0];
-        return L - posledniy;
-    }
-
-    public static int DlinnaMezhduSvetoforami(int firstsvet, int secondsvet, int[][] track) {
-        int result = 0;
-        for (int i = 0; i < 2; i++) {
-            result = track[secondsvet][0] - track[firstsvet][0];
-        }
-        return result;
-    }
-
-
-    //Создаем массив объектов светофоров
     public static Svetofors[] arraySvetofors(int[][] opisanie) {
         Svetofors[] svetofors = new Svetofors[opisanie.length];
         for (int i = 0; i < opisanie.length; i++) {
