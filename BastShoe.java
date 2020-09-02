559be50a6f9ba378d31f20a3a00301e1b72c97b5
esonegin <65952547+esonegin@users.noreply.github.com>
2020-09-02 21:03:51 +0300

    Update BastShoe.java

--- BastShoe.java
+++ BastShoe.java
@@ -5,73 +5,57 @@ public class Level1  {
 
     static Stroka stroka = new Stroka("");
     
- public static String BastShoe(String command) {
+public static String BastShoe(String command) {
         int com;
         String value = "";
         String result = "";
-
-       
         if (command.contains(" ")) {
             com = Integer.parseInt(command.split(" ")[0]);
             value = command.substring(2);
         } else {
             com = Integer.parseInt(command);
         }
-
-       
         if (com == 1) {
             command1(value);
-            
         } else if (com == 2) {
             command2(value);
-
-           
         } else if (com == 4) {
             command4();
-
         } else if (com == 5) {
             command5();
         }
-
         if (com == 1 || com == 2 || com == 4 || com == 5) {
             result = stroka.getValue();
 
         } else if (com == 3) {
             result = command3(value);
         }
-
         System.out.println(result);
-
         return result;
     }
 
     public static void command1(String value) {
         stroka.setValue(stroka.getValue() + value);
-   
         if (stroka.getPredoperation() == 4) {
             stroka.replaceSpisokAll();
             stroka.setSpisokAll(stroka.getValueOtkat());
             stroka.setUndoCount(0);
         }
-
         stroka.setPredoperation(1);
         stroka.setSpisokAll(stroka.getValue());
     }
 
     public static void command2(String value) {
-
         if (stroka.getPredoperation() == 4) {
             stroka.replaceSpisokAll();
             stroka.setSpisokAll(stroka.getValueOtkat());
             stroka.setUndoCount(0);
         }
-
         if (Integer.parseInt(value) > stroka.getValue().length()) {
             stroka.setValue("");
         } else {
             stroka.setValue(stroka.getValue().substring(0, stroka.getValue().length() - Integer.parseInt(value)));
         }
-
         stroka.setPredoperation(2);
         stroka.setSpisokAll(stroka.getValue());
 
@@ -88,49 +72,41 @@ public class Level1  {
     }
 
     public static void command4() {
-
-        stroka.getSpisokAll();
-
-        if (stroka.getSpisokAll().size() == 1) {
+       if (stroka.getSpisokAll().size() == 1) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue("");
-
-
         } else if (stroka.getSpisokAll().size() == 2 && stroka.getUndoCount() == 0) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue(stroka.getSpisokAll().get(stroka.getSpisokAll().size() - 2));
-
         } else if (stroka.getSpisokAll().size() == 2 && stroka.getUndoCount() > 0) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue(stroka.getSpisokAll().get(0));
-
         } else if (stroka.getSpisokAll().size() > 2 && stroka.getUndoCount() == 0) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue(stroka.getSpisokAll().get(stroka.getSpisokAll().size() - 2));
-
         } else if (stroka.getSpisokAll().size() > 2 && stroka.getUndoCount() > 0 && stroka.getSpisokAll().size() >= (stroka.getUndoCount() + stroka.getRedoCount())) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue(stroka.getSpisokAll().get(stroka.getSpisokAll().size() - 2 - stroka.getUndoCount()));
         }
-
         else if (stroka.getSpisokAll().size() > 2 && stroka.getUndoCount() > 0 && stroka.getSpisokAll().size() < (stroka.getUndoCount() + stroka.getRedoCount())) {
             stroka.setSpisokUndo(stroka.getValue());
             stroka.setValue(stroka.getSpisokAll().get(0));
         }
-
         stroka.setPredoperation(4);
         stroka.setUndoCount(stroka.getUndoCount() + 1);
         stroka.setValueOtkat(stroka.getValue());
     }
 
     public static void command5() {
-
         stroka.getSpisokAll();
         if(stroka.getSpisokUndo().size() - 1 - stroka.getRedoCount() >= 0 && stroka.getSpisokAll().size() > 2) {
             stroka.setValue(stroka.getSpisokUndo().get(stroka.getSpisokUndo().size() - 1 - stroka.getRedoCount()));
         }
-        else if(stroka.getSpisokUndo().size() - 1 - stroka.getRedoCount() >= 0 && stroka.getSpisokAll().size() == 2) {
+        else if(stroka.getSpisokUndo().size() - 1 - stroka.getRedoCount() >= 0 && stroka.getSpisokAll().size() == 2 && stroka.getUndoCount() == 1) {
             stroka.setValue(stroka.getSpisokUndo().get(stroka.getSpisokUndo().size() - stroka.getUndoCount()));
+        }
+        else if(stroka.getSpisokUndo().size() - 1 - stroka.getRedoCount() >= 0 && stroka.getSpisokAll().size() == 2 && stroka.getUndoCount() > 1) {
+            stroka.setValue(stroka.getSpisokUndo().get(stroka.getSpisokUndo().size() - stroka.getRedoCount()));
         }
         else{
             stroka.setValue(stroka.getValue());
@@ -138,7 +114,6 @@ public class Level1  {
         stroka.setSpisokAll(stroka.getValue());
         stroka.setRedoCount(stroka.getRedoCount() + 1);
         stroka.setPredoperation(5);
-
 
     }
 
@@ -154,67 +129,51 @@ public class Level1  {
         public Stroka(String value) {
             this.value = value;
         }
-
         public String getValue() {
             return value;
         }
-
         public int getPredoperation() {
             return predoperation;
         }
-
         public ArrayList<String> getSpisokAll() {
             return spisokAll;
         }
-
         public ArrayList<String> getSpisokUndo() {
             return spisokUndo;
         }
-
         public int getUndoCount() {
             return undoCount;
         }
-
         public int getRedoCount() {
             return redoCount;
         }
-
         public String getValueOtkat() {
             return valueOtkat;
         }
-
         public void setValue(String value) {
             this.value = value;
         }
-
         public void setPredoperation(int predoperation) {
             this.predoperation = predoperation;
         }
-
         public void setSpisokAll(String stroka) {
             spisokAll.add(stroka);
         }
-
         public void setSpisokUndo(String stroka) {
             spisokUndo.add(stroka);
         }
-
         public void setUndoCount(int undoCount) {
             this.undoCount = undoCount;
         }
-
         public void setRedoCount(int redoCount) {
             this.redoCount = redoCount;
         }
-
         public void setValueOtkat(String valueOtkat) {
             this.valueOtkat = valueOtkat;
         }
-
         public void replaceSpisokAll() {
             spisokAll.clear();
         }
-
-
     }
 }
+
